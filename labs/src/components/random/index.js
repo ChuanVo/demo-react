880ddf094fb768cbc557df4f7c9829dda9acb094
ChuanVo <chuanthinhvo@gmail.com>
2023-09-21 23:40:48 +0700

    random number v2

--- labs/src/components/random/index.js
+++ labs/src/components/random/index.js
@@ -1,23 +1,55 @@
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./style.css"
 
 
 function RandomNumber() {
+    const [min, setMin] = useState(0)
+    const [max, setMax] = useState(0)
+    const [random, setRandom] = useState(0)
+
+    function handleMinChange(event) {
+        setMin(event.target.value)
+        // const value = event.target.value
+        // setMin(Number(value))
+    }
+
+    const handleMaxChange = (event) => {
+        console.log(event)
+        // const value = event.target.value
+        // setMax(Number(value))
+    } 
+
+    function handleGenerateButtonClick() {
+        const value = Math.floor(Math.random() * (max - min)) + min;
+        setRandom(value)
+    }
+
 
 
     return(
         <div className='wrapper-random-number'>
             <div className='wrapper-first-row'>
                 <div className='random-result'>
-                    2
+                    {random}
                 </div>
                 <div className='wrapper-min-max'>
-                    <TextField className='wrapper-min' id="standard-basic" label="min" variant="standard" />
-                    <TextField className='wrapper-max' id="standard-basic" label="max" variant="standard" />
+                    <TextField 
+                        className='wrapper-min' 
+                        id="standard-basic" 
+                        label="min" 
+                        variant="standard" 
+                        onChange={handleMinChange}/>
+                    <TextField 
+                        className='wrapper-max' 
+                        id="standard-basic" 
+                        label="max" 
+                        variant="standard" 
+                        onChange={handleMaxChange}/>
                 </div>
             </div>
-            <Button className='random-button' variant="contained">Generate</Button>
+            <Button className='random-button' variant="contained" onClick={handleGenerateButtonClick}>Generate</Button>
 
         </div>
     )
